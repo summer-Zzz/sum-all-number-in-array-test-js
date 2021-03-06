65063674dac89ebc89c64399249fb5792fb11d21
Hongjin Zhu <summerellaooo@gmail.com>
2021-03-06 22:21:12 +0000

    update sumitems function

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
+const arr = [1, 2, [3, 4], 5];
+
 function sumItems(array) {
   // Sum all the numbers in the array
+  let sum = 0;
+  array.forEach((item) => {
+    if (Array.isArray(item)) {
+      sum += sumItems(item);
+    } else {
+      sum += item;
+    }
+  });
+  return sum;
 }
+
+module.exports = sumItems;
+
+console.log(sumItems(arr));
+
+
 
 module.exports = sumItems;
